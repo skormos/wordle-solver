2b2680387a64996a0b9719862cdea2abc9c9a91c
Spencer Kormos <spencer@kormos.net>
2024-07-13 13:39:41 -0400

    include previous answers. minor clean up.

--- cmd/solver/main.go
+++ cmd/solver/main.go
@@ -39,8 +39,85 @@ func (s *alphabetSet) String() string {
 	return strings.Join(letters, "")
 }
 
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	allowedWords, err := allowedList("allowed.txt")
+	if err != nil {
+		logger.Error("Could not read allowed list.", "error", err)
+		os.Exit(1)
+	}
+
+	testList := make([]string, 0, len(allowedWords))
+
+	answers, err := answersSet("answers.txt")
+	if err != nil {
+		logger.Error("Could not read answers list.", "error", err)
+		os.Exit(1)
+	}
+
+	for _, word := range allowedWords {
+		if _, ok := answers[word]; !ok {
+			testList = append(testList, word)
+		}
+	}
+
+	alphaSets := make([]alphabetSet, 5)
+	for i := 0; i < 5; i++ {
+		alphaSets[i] = newAlphabetSet()
+	}
+
+	containsSet := make(map[string]struct{})
+	for i := 0; i < 6; i++ {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(fmt.Sprintf("%d > ", i+1))
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			logger.Error("Could not read input.", "error", err)
+			os.Exit(1)
+		}
+
+		input := strings.Split(text[:len(text)-1], " ")
+		if len(input) == 1 || input[0] == input[1] {
+			_, _ = fmt.Fprintf(os.Stdout, "Congratulations!")
+			os.Exit(0)
+		}
+		for c := 0; c < 5; c++ {
+			if string(input[1][c]) == string(input[0][c]) { // letter is in the correct spot
+				alphaSets[c].keepOnly(string(input[0][c]))
+			} else if string(input[1][c]) == "*" { // letter is in the word, not this spot
+				containsSet[string(input[0][c])] = struct{}{}
+				alphaSets[c].delete(string(input[0][c]))
+			} else if string(input[1][c]) == "_" { // letter is not in this word
+				for _, set := range alphaSets {
+					set.delete(string(input[0][c]))
+				}
+			}
+		}
+
+		remaining := make([]string, 0, len(testList))
+		logger.Info(calcRegex(alphaSets))
+		pattern := regexp.MustCompile("^" + calcRegex(alphaSets) + "$")
+	words:
+		for _, word := range testList {
+			if pattern.MatchString(word) {
+				for a := range containsSet {
+					if !strings.Contains(word, a) {
+						continue words
+					}
+				}
+				remaining = append(remaining, word)
+				_, _ = fmt.Fprintf(os.Stdout, "%s\n", word)
+			}
+		}
+		testList = remaining
+		sort.Strings(testList)
+		_, _ = fmt.Fprintf(os.Stdout, "--- Remaining words %d\n", len(testList))
+	}
+}
+
 func allowedList(filename string) ([]string, error) {
-	b, err := os.ReadFile("allowed.txt")
+	b, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("reading file %w", err)
 	}
@@ -56,66 +133,21 @@ func allowedList(filename string) ([]string, error) {
 	return allowedWords, nil
 }
 
-func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
-	allowedWords, err := allowedList("allowed.txt")
+func answersSet(filename string) (map[string]struct{}, error) {
+	b, err := os.ReadFile(filename)
 	if err != nil {
-		logger.Error("Could not read allowed list.", "error", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("reading file %w", err)
 	}
-
-	alphaSets := make([]alphabetSet, 5)
-	for i := 0; i < 5; i++ {
-		alphaSets[i] = newAlphabetSet()
+	previousWords := make(map[string]struct{})
+	scanner := bufio.NewScanner(strings.NewReader(string(b)))
+	for scanner.Scan() {
+		word := strings.Split(strings.ToUpper(scanner.Text()), " ")[0]
+		previousWords[word] = struct{}{}
 	}
-
-	containsSet := make(map[string]struct{})
-	for i := 0; i < 6; i++ {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(fmt.Sprintf("%d > ", i+1))
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
-
-		input := strings.Split(text[:len(text)-1], " ")
-		if len(input) == 1 || input[0] == input[1] {
-			logger.Info("Congratulations!")
-			os.Exit(0)
-		}
-		for c := 0; c < 5; c++ {
-			if string(input[1][c]) == string(input[0][c]) { // letter is in the right spot
-				alphaSets[c].keepOnly(string(input[0][c]))
-			} else if string(input[1][c]) == "*" { // letter is in the word, not this spot
-				containsSet[string(input[0][c])] = struct{}{}
-				alphaSets[c].delete(string(input[0][c]))
-			} else if string(input[1][c]) == "_" { // letter is not in this word
-				for _, set := range alphaSets {
-					set.delete(string(input[0][c]))
-				}
-			}
-		}
-
-		remaining := make([]string, 0, len(allowedWords))
-		fmt.Println(calcRegex(alphaSets))
-		pattern := regexp.MustCompile("^" + calcRegex(alphaSets) + "$")
-	words:
-		for _, word := range allowedWords {
-			if pattern.MatchString(word) {
-				for a := range containsSet {
-					if !strings.Contains(word, a) {
-						continue words
-					}
-				}
-				remaining = append(remaining, word)
-				fmt.Println(word)
-			}
-		}
-		allowedWords = remaining
-		fmt.Println(len(allowedWords))
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("splitting lines: %w", err)
 	}
-
+	return previousWords, nil
 }
 
 func calcRegex(sets []alphabetSet) string {
